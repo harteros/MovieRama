06ce4b0340f41056cd2e1e623800cb2d2d910cef
Lefteris Harteros <l.harteros@gmail.com>
2021-09-05 17:06:39 +0300

    updated movie view and movie model

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.conf import settings
 from django.db import models
 
@@ -23,22 +21,3 @@ class Movie(models.Model):
             Returns a movie by its title
         """
         return self.title
-
-    def count_likes(self):
-        """
-            Returns the number of users that like the movie
-        """
-        return self.likes.all().count()
-
-    def count_hates(self):
-        """
-            Returns the number of users that hate the movie
-        """
-        return self.hates.all().count()
-
-    def days_ago(self):
-        """
-            Returns the number of days before which the movie was published
-        """
-        days_ago = datetime.datetime.now(tz=None) - self.pub_date.replace(tzinfo=None)
-        return days_ago.days
